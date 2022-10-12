3cd7a92efb45f433e5874233d89cce643f7c0cc5
voronov <voronov54@gmail.com>
2022-10-12 14:32:20 +0700

    code cleaning

--- scripts/docker-postgresql-multiple-databases/create-multiple-postgresql-databases.sh
+++ scripts/docker-postgresql-multiple-databases/create-multiple-postgresql-databases.sh
@@ -3,20 +3,21 @@
 set -e
 set -u
 
-function create_user_and_database() {
+function create_database() {
 	local database=$1
-	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
+	echo "  Creating database '$database' \n"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB"<<-EOSQL
 	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
+	echo "  Created database '$database' \n"
+	echo "  Granted all priveleges on '$database' to '$POSTGRES_USER' \n"
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_user_and_database $db
+		create_database $db
 	done
 	echo "Multiple databases created"
 fi
